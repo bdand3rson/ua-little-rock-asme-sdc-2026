a292a16bde48421890be2327d9916f24b286bea7
bdand3rson <bdanderson@ualr.edu>
2026-03-15 19:00:25 -0500

    new file:   xbox-input.py 	new file:   xbox-pi-to-arduino.ino/xbox-pi-to-arduinov3.ino.ino

---
+++ xbox-input.py
@@ -0,0 +1,58 @@
+import pygame
+import serial
+import time
+
+# Connect to Arduino
+arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+time.sleep(2)
+
+# Initialize Xbox controller
+pygame.init()
+pygame.joystick.init()
+
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+
+print("Controller connected:", joystick.get_name())
+
+def map_axis_to_speed(value):
+    """Convert joystick axis (-1 to 1) to PWM (0 to 255)."""
+    return int(abs(value) * 255)
+
+try:
+    while True:
+        pygame.event.pump()
+
+        y_axis = joystick.get_axis(1)  # Forward/back
+        x_axis = joystick.get_axis(3)  # Turn
+
+        cmd = 'S'
+        speed = 0
+
+        if abs(y_axis) > 0.2:  # Forward/back control
+            speed = map_axis_to_speed(y_axis)
+            if y_axis < 0:
+                cmd = 'F'
+            else:
+                cmd = 'B'
+        elif abs(x_axis) > 0.2:  # Left/right turn
+            speed = map_axis_to_speed(x_axis)
+            if x_axis < 0:
+                cmd = 'L'
+            else:
+                cmd = 'R'
+        else:
+            cmd = 'S'
+            speed = 0
+
+        message = f"{cmd},{speed}\n"
+        arduino.write(message.encode())
+        print(f"Sent: {message.strip()}")
+
+        time.sleep(0.05)
+
+except KeyboardInterrupt:
+    print("Exiting...")
+    arduino.write(b"S,0\n")
+    arduino.close()
+    pygame.quit()
---
+++ xbox-pi-to-arduino.ino/xbox-pi-to-arduinov3.ino.ino
@@ -0,0 +1,84 @@
+#define DIR_M1 22
+#define PWM_M1 2
+#define DIR_M2 23
+#define PWM_M2 3
+#define DIR_M3 24
+#define PWM_M3 4
+#define DIR_M4 25
+#define PWM_M4 5
+
+void setup() {
+  pinMode(DIR_M1, OUTPUT);
+  pinMode(PWM_M1, OUTPUT);
+  pinMode(DIR_M2, OUTPUT);
+  pinMode(PWM_M2, OUTPUT);
+  pinMode(DIR_M3, OUTPUT);
+  pinMode(PWM_M3, OUTPUT);
+  pinMode(DIR_M4, OUTPUT);
+  pinMode(PWM_M4, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void setMotors(bool dir, int speed) {
+  digitalWrite(DIR_M1, dir);
+  digitalWrite(DIR_M2, dir);
+  digitalWrite(DIR_M3, dir);
+  digitalWrite(DIR_M4, dir);
+
+  analogWrite(PWM_M1, speed);
+  analogWrite(PWM_M2, speed);
+  analogWrite(PWM_M3, speed);
+  analogWrite(PWM_M4, speed);
+}
+
+void stopMotors() {
+  analogWrite(PWM_M1, 0);
+  analogWrite(PWM_M2, 0);
+  analogWrite(PWM_M3, 0);
+  analogWrite(PWM_M4, 0);
+}
+
+void loop() {
+  static String input = "";
+
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c == '\n') {
+      // Parse message format: DIRECTION,SPEED
+      int commaIndex = input.indexOf(',');
+      if (commaIndex > 0) {
+        char cmd = input.charAt(0);
+        int speed = input.substring(commaIndex + 1).toInt();
+        speed = constrain(speed, 0, 255);
+
+        if (cmd == 'L') setMotors(LOW, speed);
+        else if (cmd == 'R') setMotors(HIGH, speed);
+        else if (cmd == 'B') { // turn left
+          digitalWrite(DIR_M1, HIGH);
+          digitalWrite(DIR_M2, LOW);
+          digitalWrite(DIR_M3, HIGH);
+          digitalWrite(DIR_M4, LOW);
+          analogWrite(PWM_M1, speed);
+          analogWrite(PWM_M2, speed);
+          analogWrite(PWM_M3, speed);
+          analogWrite(PWM_M4, speed);
+        }
+        else if (cmd == 'F') { // turn right
+          digitalWrite(DIR_M1, LOW);
+          digitalWrite(DIR_M2, HIGH);
+          digitalWrite(DIR_M3, LOW);
+          digitalWrite(DIR_M4, HIGH);
+          analogWrite(PWM_M1, speed);
+          analogWrite(PWM_M2, speed);
+          analogWrite(PWM_M3, speed);
+          analogWrite(PWM_M4, speed);
+        }
+        else if (cmd == 'S') stopMotors();
+      }
+      input = "";
+    } else {
+      input += c;
+    }
+  }
+}
